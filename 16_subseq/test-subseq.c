aeec5324454b960c1d1ed76c4c8159711caa5dfb
Coursera Learner <nobody@nowhere.nul>
2020-07-19 19:12:54 +0000

    m

--- 16_subseq/test-subseq.c
+++ 16_subseq/test-subseq.c
@@ -6,15 +6,16 @@ size_t maxSeq(int *array, size_t n);
 int main(void){
   int a[1] = {0};
   int b[4] = {1,2,5,0};
-  int c[10] = {1,2,3,1,2,3,1,2,3,4};
+  int c[18] = {1,2,3,4,3,2,0,3,4,5,6,7,8,3,2,5,6,7};
   int d[3] = {0,0,0};
   if (maxSeq(a,0) !=0)
     printf("aa");
   if (maxSeq(b,4) != 3)
     printf("bb");
-  if (maxSeq(c,10) != 4)
+  if (maxSeq(c,18) != 7)
     printf("cc");
   if (maxSeq(d,3) != 1)
     printf("dd");
+  printf("success\n");
   return EXIT_SUCCESS;
 }
