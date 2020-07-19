70aa1801712e2f40452d64be53e50849d61b5083
Coursera Learner <nobody@nowhere.nul>
2020-07-19 17:55:30 +0000

    Finished

--- 16_subseq/maxSeq.c
+++ 16_subseq/maxSeq.c
@@ -1,13 +1,13 @@
 
 int maxSeq(int * array, int  n){
-  double length = 0,maxL=0;
+  double length = 1,maxL=1;
   for (int i=0; i < n; i++){
     if (array[i]<array[i+1])
       length++;
     else{
       if (length > maxL)
 	maxL = length;
-      length =0;
+      length =1;
     }
   }
   if (length > maxL)
